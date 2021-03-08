530474118283336ba06b3478e385d90b442fd251
Morteza Habibzadeh <morteza.habibzadeh.contact@gmail.com>
2021-03-08 17:49:22 +0300

    Native ad in Exit AlertDialog sample codes (JavaScript - Gradle 19.7.0)

--- app/src/main/java/com/example/nativeadproject/MainActivity.java
+++ app/src/main/java/com/example/nativeadproject/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.nativeadproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.ads.AdListener;
@@ -17,6 +21,8 @@ import com.google.android.gms.ads.nativead.NativeAd;
 import com.google.android.gms.ads.nativead.NativeAdOptions;
 
 public class MainActivity extends AppCompatActivity {
+    NativeAd mNativeAd;
+
 
     private static final String TAG = "--->Native Ad";
 
@@ -24,9 +30,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG, "Native Ad Project runs");
-
-        TemplateView template = findViewById(R.id.my_template);
+        Log.d(TAG, "------Native Ad Project runs------");
 
         //---> initializing Google Ad SDK
         MobileAds.initialize(this, new OnInitializationCompleteListener() {
@@ -34,46 +38,108 @@ public class MainActivity extends AppCompatActivity {
             public void onInitializationComplete(InitializationStatus initializationStatus) {
                 Log.d(TAG, "Google SDK Initialized");
 
-
-                AdLoader adLoader = new AdLoader.Builder(MainActivity.this, "ca-app-pub-3940256099942544/2247696110")
-
-                        .forNativeAd(new NativeAd.OnNativeAdLoadedListener() {
-                            @Override
-                            public void onNativeAdLoaded(NativeAd nativeAd) {
-                                Log.d(TAG, "Native Ad Loaded");
-
-                                if (isDestroyed()) {
-                                    nativeAd.destroy();
-                                    Log.d(TAG, "Native Ad Destroyed");
-                                    return;
-                                }
-
-                                NativeTemplateStyle styles = new
-                                        NativeTemplateStyle.Builder().build();
-
-
-                                template.setStyles(styles);
-                                template.setVisibility(View.VISIBLE);
-                                template.setNativeAd(nativeAd);
-
-                            }
-                        })
-
-                        .withAdListener(new AdListener() {
-                            @Override
-                            public void onAdFailedToLoad(LoadAdError adError) {
-                                // Handle the failure by logging, altering the UI, and so on.
-                                Log.d(TAG, "Native Ad Failed To Load");
-                                template.setVisibility(View.GONE);
-
-                            }
-                        })
-                        .withNativeAdOptions(new NativeAdOptions.Builder()
-                                .build())
-                        .build();
-
-                adLoader.loadAd(new AdRequest.Builder().build());
+                loadNativeAd();
             }
         });
+
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        final AlertDialog.Builder exitBuilderDialog = new AlertDialog.Builder(this);
+
+        View dialogView = getLayoutInflater().inflate(R.layout.exit_layout, null);
+
+        Button yesBtm = dialogView.findViewById(R.id.id_exit_pos_btm);
+        Button noBtm= dialogView.findViewById(R.id.id_exit_neg_btm);
+        TextView txtTitle = dialogView.findViewById(R.id.id_exit_title);
+        TextView txtMessage = dialogView.findViewById(R.id.id_exit_message);
+        TemplateView template = dialogView.findViewById(R.id.my_template);
+
+        template.setVisibility(View.GONE);
+
+        if(mNativeAd!=null) {
+            NativeTemplateStyle styles = new
+                    NativeTemplateStyle.Builder().build();
+
+
+            template.setStyles(styles);
+            template.setVisibility(View.VISIBLE);
+            template.setNativeAd(mNativeAd);
+        }
+
+
+
+        txtTitle.setText("Exit");
+        txtMessage.setText("Do you want to exit?");
+
+        exitBuilderDialog.setView(dialogView);
+        final AlertDialog alertDialog = exitBuilderDialog.create();
+        alertDialog.setCanceledOnTouchOutside(false);
+
+        alertDialog.show();
+
+        yesBtm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intentExit = new Intent(Intent.ACTION_MAIN);
+                intentExit.addCategory(Intent.CATEGORY_HOME);
+                intentExit.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intentExit);
+
+                finish();
+                System.exit(0);
+            }
+        });
+
+        noBtm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mNativeAd!=null){
+                    mNativeAd.destroy();
+                }
+                loadNativeAd();
+                alertDialog.cancel();
+            }
+        });
+
+    }
+
+    private void loadNativeAd(){
+        AdLoader adLoader = new AdLoader.Builder(MainActivity.this, "ca-app-pub-3940256099942544/2247696110")
+
+                .forNativeAd(new NativeAd.OnNativeAdLoadedListener() {
+                    @Override
+                    public void onNativeAdLoaded(NativeAd nativeAd) {
+                        Log.d(TAG, "Native Ad Loaded");
+
+
+                        if (isDestroyed()) {
+                            nativeAd.destroy();
+                            Log.d(TAG, "Native Ad Destroyed");
+                            return;
+                        }
+
+                        mNativeAd=nativeAd;
+
+                    }
+                })
+
+                .withAdListener(new AdListener() {
+                    @Override
+                    public void onAdFailedToLoad(LoadAdError adError) {
+                        // Handle the failure by logging, altering the UI, and so on.
+                        Log.d(TAG, "Native Ad Failed To Load");
+
+                    }
+                })
+                .withNativeAdOptions(new NativeAdOptions.Builder()
+                        .build())
+                .build();
+
+        adLoader.loadAd(new AdRequest.Builder().build());
+
     }
 }
